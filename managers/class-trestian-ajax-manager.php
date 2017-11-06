73e3fde90bf5566292e499f57ffc0a255fa14786
Yaron Guez <yaron@trestian.com>
2017-11-05 17:25:33 -0800

    New AJAX commands

--- managers/class-trestian-ajax-manager.php
+++ managers/class-trestian-ajax-manager.php
@@ -50,24 +50,79 @@ class Trestian_Ajax_Manager{
     }
 
 	/**
-	 * Fetches and optionally sanitizes data from POST while triggering error if missing
-	 * @param $field
-	 * @param $message
-	 * @param bool $sanitize_text
+	 * Fetches and optionally sanitizes data while triggering error if missing
+	 *
+	 * @param $field - field name to check
+	 * @param $message - error message to return on failure
+	 * @param bool $sanitize_text - whether or not to sanitize the data property as well
+	 * @param null $data - data source. defaults to $_REQUEST
 	 *
 	 * @return string
 	 */
-    public function check_missing_data($field, $message, $sanitize_text = true)
+    public function check_missing_data($field, $message, $sanitize_text = true, $data = null)
     {
-        if (!isset($_POST[$field]) || !strlen($_POST[$field])) {
+    	if($data == null){
+    		$data = $_REQUEST;
+	    }
+
+        if (
+        	!isset($data[$field]) ||
+            (is_array($data[$field]) && empty($data[$field])) ||
+            (!is_array($data[$field]) && !strlen($data[$field]))
+        ) {
             $this->return_error($message);
         } else if($sanitize_text){
-        	return sanitize_text_field($_POST[$field]);
+        	return $this->sanitize_text($data[$field]);
         }
         else {
-            return $_POST[$field];
+            return $data[$field];
         }
     }
+
+    private function sanitize_text($field){
+    	if(is_array($field)){
+    		foreach($field as $i => $value){
+    			$field[$i] = $this->sanitize_text($value);
+		    }
+		    return $field;
+	    }
+
+	    return sanitize_text_field($field);
+    }
+
+	/**
+	 * Fetches and optionally sanitizes data from POST while triggering error if missing
+	 *
+	 * @param $field - Which POST field to check
+	 * @param $message - Error message when field is missing
+	 * @param string $format - Date format expected
+	 * @param string $invalid_date_message - Error to return on invalid date
+
+	 *
+	 * @return DateTime
+	 */
+	public function check_missing_date($field, $message, $format = 'Y-m-d', $invalid_date_message = 'Invalid date specified', $data = null, $returnDateTime = true)
+	{
+		if($data == null){
+			$data = $_REQUEST;
+		}
+		if (!isset($data[$field]) || !strlen($data[$field])) {
+			$this->return_error($message);
+		}
+		$val = sanitize_text_field($data[$field]);
+
+		$date = DateTime::createFromFormat($format, $val);
+		if(!$date || $date->format($format) !== $val){
+			$this->return_error($invalid_date_message);
+		}
+
+		if($returnDateTime) {
+			return $date;
+		} else {
+			return $val;
+		}
+	}
+
 
 	/**
 	 * Helper function to validate nonce in form submission
@@ -78,10 +133,10 @@ class Trestian_Ajax_Manager{
 	 * @return string - action of form
 	 */
     public function validate_nonce($nonce_field = 'nonce', $action_field = 'action', $message='The form has expired. Please refresh the page and try again'){
-    	if(!isset($_POST[$nonce_field]) || !isset($_POST[$action_field]) || !wp_verify_nonce($_POST[$nonce_field], $_POST[$action_field])){
+    	if(!isset($_REQUEST[$nonce_field]) || !isset($_REQUEST[$action_field]) || !wp_verify_nonce($_REQUEST[$nonce_field], $_REQUEST[$action_field])){
     		$this->return_error($message);
 	    }
 
-	    return $_POST[$action_field];
+	    return $_REQUEST[$action_field];
     }
 }
--- trestian-wp-managers.php
+++ trestian-wp-managers.php
@@ -9,7 +9,7 @@
  * Plugin Name:         Trestian WP Managers
  * Plugin URI:          https://github.com/yaronguez/trestian-wp-managers
  * Description:         Set of shared libraries and tools for plugin development
- * Version:             1.0.16
+ * Version:             1.0.17
  * Author:              Yaron Guez
  * Author URI:          http://trestian.com
  * License:             GPL-2.0+
